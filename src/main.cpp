e3b27075375bc48a6248f274b37985b0d3a17b78
smolck <46855713+smolck@users.noreply.github.com>
2025-02-12 14:30:59 -0600

    its squarin time

--- src/main.cpp
+++ src/main.cpp
@@ -4,35 +4,116 @@
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
-int main()
-{
-  fmt::println("Hello, World!");
+const char *VERTEX_SHADER = "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\0";
+const char *FRAGMENT_SHADER = "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n\0";
 
-    GLFWwindow* window;
 
-    if (!glfwInit())
-        return -1;
+void framebufferSizeCallback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
 
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
-        glfwTerminate();
-        return -1;
-    }
+GLFWwindow* initialSetup() {
+  if (!glfwInit()) return NULL;
+  GLFWwindow* window;
 
-    glfwMakeContextCurrent(window);
-    gladLoadGL();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    while (!glfwWindowShouldClose(window))
-    {
-        glClear(GL_COLOR_BUFFER_BIT);
-        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
-
-        glfwSwapBuffers(window);
-
-        glfwPollEvents();
-    }
-
+  window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
+  if (!window) {
     glfwTerminate();
-    return 0;
+    return NULL;
+  }
+
+  glfwMakeContextCurrent(window);
+  gladLoadGL();
+
+  return window;
+}
+
+int main() {
+  GLFWwindow* window = initialSetup();
+  if (window == NULL) { return -1; }
+
+  glViewport(0, 0, 800, 600);
+
+  uint32_t vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  uint32_t fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+  glShaderSource(vertexShader, 1, &VERTEX_SHADER, NULL);
+  glCompileShader(vertexShader);
+  glShaderSource(fragmentShader, 1, &FRAGMENT_SHADER, NULL);
+  glCompileShader(fragmentShader);
+
+  uint32_t shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glLinkProgram(shaderProgram);
+
+  int success;
+  char infoLog[512];
+
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success) {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    fmt::println("ERROR::SHADER::PROGRAM::LINKING_FAILED\n {}", infoLog);
+  }
+
+  glUseProgram(shaderProgram);
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+      float vertices[] = {
+         0.5f,  0.5f, 0.0f,  // top right
+         0.5f, -0.5f, 0.0f,  // bottom right
+        -0.5f, -0.5f, 0.0f,  // bottom left
+        -0.5f,  0.5f, 0.0f   // top left 
+    };
+    unsigned int indices[] = {  // note that we start from 0!
+        0, 1, 3,  // first Triangle
+        1, 2, 3   // second Triangle
+    };
+
+  uint32_t vbo, vao, ebo;
+  glGenVertexArrays(1, &vao);
+  glGenBuffers(1, &vbo);
+  glGenBuffers(1, &ebo);
+
+  glBindVertexArray(vao);
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(vao);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
 }
